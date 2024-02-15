5d2c938992f3d6c61804cfc1a114774013c5c93b
Jonathan Binder <jonathanbinder@web.de>
2024-02-15 14:13:20 +0100

    Changing Persons.jsx and persons.js to work with the nodejs - express backend

--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -1,7 +1,7 @@
 import Person from "./Person"
 
 const Persons = ({newFilter, persons, deleteEntry}) => {
-    return (
+  return (
       persons.filter((person) => person.name.toLowerCase().includes(newFilter.toLowerCase())).map(person => <Person key={person.id} person={person} deleteEntry={deleteEntry}/>)
     )
   }
