b9d3083f54cf4f5c9002da079e16448cc585facc
Jonathan Binder <jonathanbinder@web.de>
2025-05-16 15:57:21 +0200

    Fresh restart

--- part2/phonebook/src/components/Person.jsx
+++
@@ -1,8 +0,0 @@
-const Person = ({person, deleteEntry}) => {
-    return (
-      <span>{person.name} {person.number} 
-      <button onClick={deleteEntry} id={person.id}>delete</button><br/></span>
-    )
-  }
-
-export default Person
--- part2/phonebook/src/components/Persons.jsx
+++
@@ -1,9 +0,0 @@
-import Person from "./Person"
-
-const Persons = ({newFilter, persons, deleteEntry}) => {
-  return (
-      persons.filter((person) => person.name.toLowerCase().includes(newFilter.toLowerCase())).map(person => <Person key={person.id} person={person} deleteEntry={deleteEntry}/>)
-    )
-  }
-
-export default Persons
